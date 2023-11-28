2f522ccc1767fe3e55cf86a4e30bf03360492dea
thanhninh <thanhninhdhcnhn@gmail.com>
2023-11-28 14:48:09 +0700

    - Update trigger condition for event system

--- Assets/_Game/Scripts/Event/TriggerCondition.cs
+++ Assets/_Game/Scripts/Event/TriggerCondition.cs
@@ -14,54 +14,83 @@ public class TriggerCondition : ScriptableObject
     public int gemCond;
     public int seashellCond;
     public int magicScrollCond;
+    Player player;
 
     public bool CheckCondition()
     {
+        player = GameObject.FindObjectOfType<Player>();
         // Todo: Check condition - wip
         if (healthCond > 0)
         {
             Debug.LogFormat("Check condition health >= {0}", healthCond);
-            return false;
+            if (player.Health < healthCond)
+            {
+                return false;
+            }
         }
         if (luckCond > 0)
         {
             Debug.LogFormat("Check condition luck >= {0}", luckCond);
-            return false;
+            if (player.Luck < luckCond)
+            {
+                return false;
+            }
         }
         if (sanityCond > 0)
         {
             Debug.LogFormat("Check condition sanity >= {0}", sanityCond);
-            return false;
+            if (player.Santity < sanityCond)
+            {
+                return false;
+            }
         }
         if (meatCond > 0)
         {
             Debug.LogFormat("Check condition meat >= {0}", meatCond);
-            return false;
+            if (!InventoryHolder.Instance.InventorySystem.IsItemHasEnoughQuanity(ItemType.Meat, meatCond))
+            {
+                return false;
+            }
         }
         if (logCond > 0)
         {
             Debug.LogFormat("Check condition log >= {0}", logCond);
-            return false;
+            if (!InventoryHolder.Instance.InventorySystem.IsItemHasEnoughQuanity(ItemType.Log, logCond))
+            {
+                return false;
+            }
         }
         if (cropCond > 0)
         {
             Debug.LogFormat("Check condition crop >= {0}", cropCond);
-            return false;
+            if (!InventoryHolder.Instance.InventorySystem.IsItemHasEnoughQuanity(ItemType.Crop, cropCond))
+            {
+                return false;
+            }
         }
         if (gemCond > 0)
         {
             Debug.LogFormat("Check condition gem >= {0}", gemCond);
-            return false;
+            if (!InventoryHolder.Instance.InventorySystem.IsItemHasEnoughQuanity(ItemType.Gem, gemCond))
+            {
+                return false;
+            }
         }
         if (seashellCond > 0)
         {
             Debug.LogFormat("Check condition seashell >= {0}", seashellCond);
-            return false;
+            if (!InventoryHolder.Instance.InventorySystem.IsItemHasEnoughQuanity(ItemType.Seashell, seashellCond))
+            {
+                return false;
+            }
         }
         if (magicScrollCond > 0)
         {
             Debug.LogFormat("Check condition magicScroll >= {0}", magicScrollCond);
-            return false;
+            if (!InventoryHolder.Instance.InventorySystem.IsItemHasEnoughQuanity(ItemType.MagicScroll, magicScrollCond))
+            {
+                return false;
+            }
         }
         return true;
     }
