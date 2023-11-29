9c1332bd617daff17f881d1e2914a28b676912a1
hungpdt <120432955+hungpdt@users.noreply.github.com>
2023-11-29 16:21:07 +0700

    + done diarymanager.cs

--- Assets/_Game/Scripts/Diary/DiaryManager.cs
+++ Assets/_Game/Scripts/Diary/DiaryManager.cs
@@ -5,50 +5,37 @@ using UnityEngine;
 
 public class DiaryManager : MonoBehaviour
 {
-
-    public List<ConsequenceData> grassConsequenceList;
-    public List<ConsequenceData> seaConsequenceList;
-    public List<ConsequenceData> desertConsequenceList;
-    public List<ConsequenceData> woodConsequenceList;
+    public List<ConsequenceData> grassConsequenceList = new List<ConsequenceData>();
+    public List<ConsequenceData> seaConsequenceList = new List<ConsequenceData>();
+    public List<ConsequenceData> desertConsequenceList = new List<ConsequenceData>();
+    public List<ConsequenceData> woodConsequenceList = new List<ConsequenceData>();
 
     public void AddConsequence(ConsequenceData consequenceData, BlockType blockType){
         switch (blockType)
         {
-            case BlockType.Grass:
-            /*if(grassConsequenceList == null){
-                grassConsequenceList.Add(consequenceData);
-            }else if (IsConsequenceExist(...){
-                    grassConsequenceList.Add(consequenceData);
-                }
-            }
+            case BlockType.Grass:            
+                AddConsequenceList(consequenceData, grassConsequenceList);
+                break;
             
-            */
-                if(IsConsequenceExist(consequenceData, BlockType.Grass)){
-                    grassConsequenceList.Add(consequenceData);
-                } else{
-                    Debug.Log(" ");
-                }
+            case BlockType.Sea:
+                AddConsequenceList(consequenceData, seaConsequenceList);
                 break;
             
             case BlockType.Desert:
-            case BlockType.Sea:
+                AddConsequenceList(consequenceData,  desertConsequenceList);
+                break;
+
             case BlockType.Wood:
+                AddConsequenceList(consequenceData, woodConsequenceList);
                 break;
         }
     }
-
-    public bool IsConsequenceExist(ConsequenceData consequenceData, BlockType blockType){
-        switch (blockType)
-        {
-            case BlockType.Grass:
-                //consequenceData.consequenceCode;
-                break;
-            
-            case BlockType.Desert:
-            case BlockType.Sea:
-            case BlockType.Wood:
-                break;
+    public void AddConsequenceList(ConsequenceData consequenceData, List<ConsequenceData> consequenceList){
+        if(!consequenceList.Any(consequence => consequence.consequenceCode == consequenceData.consequenceCode)){
+            consequenceList.Add(consequenceData);
+            Debug.Log($"Consequence {consequenceData.consequenceCode} added to the list");
+        } else{
+            Debug.Log($"Consequence {consequenceData.consequenceCode} is exist or don't have in list");
         }
-        return true;
     }
 }
